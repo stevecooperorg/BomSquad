b01af1c33046f66c325b9b9bebfcc3046d880d6d
Steve Cooper <steve@stevecooper.org>
2014-05-27 22:36:11 +0100

    totally untested build task. Do not use!

---
+++ Source/BomSquad/BomSquad.BuildTasks/BomSquadTask.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+
+namespace BomSquad.BuildTasks
+{
+    public class BomSquadTask: Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public bool ShouldHaveBom { get; set; }
+
+
+        public ITaskItem[] SourceCodeFiles
+        {
+            get;
+            set;
+        }
+
+        public BomSquadTask()
+        {
+            // by default, the files searched SHOULD have a BOM. Mark as false in order to ensure that
+            // files do not have a BOM. 
+           this.ShouldHaveBom = true;
+        }
+
+        public override bool Execute()
+        {
+            List<string> sourceCodeFiles = new List<string>();
+            if (this.SourceCodeFiles != null)
+            {
+                sourceCodeFiles.AddRange(this.SourceCodeFiles.Select(it => it.ItemSpec));
+            }
+
+            var dog = new SnifferDog();
+            var failed = false;
+            foreach (var sourceCodeFile in sourceCodeFiles)
+            {
+                Log.LogMessageFromText("Checking " + sourceCodeFile, MessageImportance.High);
+                var asExpected = dog.DetectBom(sourceCodeFile) == this.ShouldHaveBom;
+                if (!asExpected)
+                {
+                    Log.LogError("'" + sourceCodeFile + "' has the wrong kind of BOM");
+                    failed = true;
+                }
+            }
+
+            return !failed;
+        }
+    }
+}
