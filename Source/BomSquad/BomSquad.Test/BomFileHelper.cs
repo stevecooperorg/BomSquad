4cda80756a52a84492b38de399a1f1c86b8fea88
Steve Cooper <steve@stevecooper.org>
2014-05-28 21:25:12 +0100

    some tests around the custom task

---
+++ Source/BomSquad/BomSquad.Test/BomFileHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomSquad.Test
+{
+    public class BomFileHelper
+    {
+        public string CreateFileWithBom(string bomType)
+        {
+            var filePath = Path.Combine(Path.GetTempPath(), "FileWith" + bomType + "Bom" + Guid.NewGuid().ToString("N") + ".txt");
+
+            using (var file = File.OpenWrite(filePath))
+            {
+                if (bomType != null)
+                {
+                    var bomDefinition = BomDefinitions.GetBomDefinitions().First(bd => bd.Name == bomType);
+
+                    // write bom
+                    foreach (var byt in bomDefinition.BOM)
+                    {
+                        file.WriteByte(byt);
+                    }
+                }
+
+                // write some simple content.
+                WriteAsciiBytes(file, "foo");
+            }
+
+            return filePath;
+        }
+
+        private void WriteAsciiBytes(FileStream file, string content)
+        {
+            // write 'foo' in ascii;
+            var fooBytes = System.Text.Encoding.ASCII.GetBytes(content);
+            file.Write(fooBytes, 0, fooBytes.Length);
+        }
+    }
+}
