1e4106a36fbf185d8744a38b65ac7fc91c4ed35a
Steve Cooper <steve@stevecooper.org>
2014-05-28 21:40:22 +0100

    added a command-line file to report on BOMs in a directory structure.

---
+++ Source/BomSquad/BomSquad.Program/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomSquad.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2 || !Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Usage: BomSquad <directoryName>");
+                return;
+            }
+
+            var rootDirectory = args[0];
+            var filter = args[1];
+
+            Console.WriteLine("Checking '{0}' for files matching '{1}'.", rootDirectory, filter);
+
+            var dog = new SnifferDog();
+            var fileCount = 0;
+            foreach(var file in Directory.EnumerateFiles(rootDirectory, filter, SearchOption.AllDirectories))
+            {
+                fileCount++;
+                var bomType = dog.DetectBomType(file);
+                if (bomType != null)
+                {
+                    Console.WriteLine("Found a {0} BOM in '{1}'", bomType, file);
+                }
+            }
+
+            Console.WriteLine("{0} total files checked", fileCount);
+        }
+    }
+}
