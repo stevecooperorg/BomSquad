3d3e4b1f33e8f4c3f74c464bea833dd3bb253d14
Steve Cooper <steve@stevecooper.org>
2014-07-16 20:40:03 +0100

    forgot a couple of files!

---
+++ Source/BomSquad/BomSquad.Test/BomSquadTaskTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BomSquad.Test
+{
+    [TestClass]
+    public class BomSquadTaskTests
+    {
+        [TestMethod]
+        public void ShouldSucceedWhenRequiredBomExists()
+        {
+            bool shouldHaveBom = true;
+            string bomType = "UTF-8";
+            var expected = true;
+
+            Test(shouldHaveBom, bomType, expected);
+        }
+
+        [TestMethod]
+        public void ShouldFailWhenRequiredBomDoesNotExists()
+        {
+            bool shouldHaveBom = true;
+            string bomType = null;
+            var expected = false;
+
+            Test(shouldHaveBom, bomType, expected);
+        }
+
+        [TestMethod]
+        public void ShouldFailOnMissingBom()
+        {
+            bool shouldHaveBom = true;
+            string bomType = null;
+            var expected = false;
+
+            Test(shouldHaveBom, bomType, expected);
+        }
+
+        [TestMethod]
+        public void ShouldSucceedOnAsciiBom()
+        {
+            bool shouldHaveBom = false;
+            string bomType = null;
+            var expected = true;
+
+            Test(shouldHaveBom, bomType, expected);
+        }
+
+        private static void Test(bool shouldHaveBom, string bomType, bool expected)
+        {
+            var helper = new BomFileHelper();
+            var file = helper.CreateFileWithBom(bomType);
+
+            var task = new BomSquad.BuildTasks.BomSquadTask();
+            task.ShouldHaveBom = shouldHaveBom;
+
+
+            var buildEngine = new Mock<IBuildEngine>(MockBehavior.Strict);
+
+            buildEngine.Setup(be => be.LogMessageEvent(It.IsAny<BuildMessageEventArgs>()));
+            buildEngine.Setup(be => be.ProjectFileOfTaskNode).Returns("MockFile");
+            buildEngine.Setup(be => be.LineNumberOfTaskNode).Returns(1);
+            buildEngine.Setup(be => be.ColumnNumberOfTaskNode).Returns(1);
+            buildEngine.Setup(be => be.LogErrorEvent(It.IsAny<BuildErrorEventArgs>()));
+
+            task.BuildEngine = buildEngine.Object;
+            task.SourceCodeFiles = new ITaskItem[] {
+                new TaskItem { ItemSpec = file }
+            };
+
+            var success = task.Execute();
+
+            Assert.AreEqual(expected, success);
+        }
+
+
+    }
+}
---
+++ Source/BomSquad/BomSquad/BomDefinitions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomSquad
+{
+    public static class BomDefinitions
+    {
+        private static int maxBomLength = -1; // -1 == not calculated
+
+        public static IEnumerable<BomDefinition> GetBomDefinitions()
+        {
+            yield return new BomDefinition { Name = "UTF-7", BOM = new byte[] { 0x2B, 0x2F, 0x76, 0x38, 0x2D } };
+            yield return new BomDefinition { Name = "UTF-EBCDIC", BOM = new byte[] { 0xDD, 0x73, 0x66, 0x73 } };
+            yield return new BomDefinition { Name = "UTF-7", BOM = new byte[] { 0x2B, 0x2F, 0x76, 0x39 } };
+            yield return new BomDefinition { Name = "UTF-7", BOM = new byte[] { 0x2B, 0x2F, 0x76, 0x38 } };
+            yield return new BomDefinition { Name = "UTF-7", BOM = new byte[] { 0x2B, 0x2F, 0x76, 0x2B } };
+            yield return new BomDefinition { Name = "UTF-32 (BE)", BOM = new byte[] { 0x00, 0x00, 0xFE, 0xFF } };
+            yield return new BomDefinition { Name = "UTF-32 (LE)", BOM = new byte[] { 0xFF, 0xFE, 0x00, 0x00 } };
+            yield return new BomDefinition { Name = "UTF-7", BOM = new byte[] { 0x2B, 0x2F, 0x76, 0x2F } };
+            yield return new BomDefinition { Name = "GB-18030", BOM = new byte[] { 0x84, 0x31, 0x95, 0x33 } };
+            yield return new BomDefinition { Name = "UTF-8", BOM = new byte[] { 0xEF, 0xBB, 0xBF } };
+            yield return new BomDefinition { Name = "UTF-16 (BE)", BOM = new byte[] { 0xFE, 0xFF } };
+            yield return new BomDefinition { Name = "UTF-16 (LE)", BOM = new byte[] { 0xFF, 0xFE } };
+            yield return new BomDefinition { Name = "UTF-1", BOM = new byte[] { 0xF7, 0x64, 0x4C } };
+            yield return new BomDefinition { Name = "SCSU", BOM = new byte[] { 0x0E, 0xFE, 0xFF } };
+            yield return new BomDefinition { Name = "BOCU-1", BOM = new byte[] { 0xFB, 0xEE, 0x28 } };
+        }
+
+        internal static int MaxBomLength
+        {
+            get
+            {
+                if (maxBomLength == -1)
+                {
+                    foreach (var bomDefinition in GetBomDefinitions())
+                    {
+                        maxBomLength = Math.Max(maxBomLength, bomDefinition.BOM.Length);
+                    }
+                }
+
+                return maxBomLength;
+            }
+        }
+    }
+}
